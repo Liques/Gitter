5dc0ad4a51f153a8dca555b344e5202363721995
Liques <liques@hotmail.com>
2019-05-09 14:55:32 +0100

    Attempt

--- src/Blazor.Gitter.Core/Components/Pages/Index.razor.cs
+++ src/Blazor.Gitter.Core/Components/Pages/Index.razor.cs
@@ -4,16 +4,21 @@ using Microsoft.AspNetCore.Components;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Blazor.Services;
 
 namespace Blazor.Gitter.Core.Components.Pages
 {
     public class IndexModel : ComponentBase, IDisposable
     {
         [Inject] IChatApi GitterApi { get; set; }
+        [Inject] IAuthApi GitterAuthApi { get; set; }
         [Inject] internal IAppState State { get; set; }
+        [Inject] internal IUriHelper UriHelper { get; set; }
 
         internal string ErrorMessage;
         string apiKey;
+
+        public string code { get; set; }
 
         internal string ApiKey
         {
@@ -25,12 +30,36 @@ namespace Blazor.Gitter.Core.Components.Pages
         {
             base.OnInit();
             State.GotChatUser += State_GotChatUser;
+
+            string code = "";
+
+            var uri = new Uri(UriHelper.GetAbsoluteUri());
+            code = Microsoft.AspNetCore.WebUtilities.QueryHelpers.ParseQuery(uri.Query).TryGetValue("code", out var type) ? type.First() : "";
+
+            if (!String.IsNullOrEmpty(code))
+            {
+                GetAccessToken(code);
+            }
+
+            
+        }
+
+        private async Task GetAccessToken(string exchangeToken)
+        {
+            apiKey = await GitterAuthApi.GetAccessToken(exchangeToken);
+            await SignIn(true);
         }
 
         private void State_GotChatUser(object sender, EventArgs e)
         {
             Invoke(StateHasChanged);
         }
+
+        internal void OAuth()
+        {
+            var urlLogin = GitterAuthApi.GetLoginUrl();
+            UriHelper.NavigateTo(urlLogin);
+        }            
 
         internal async Task SignIn(bool remember)
         {
